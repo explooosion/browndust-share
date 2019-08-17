66d6cc2d50ed65b79f03591eac1f3410bd3fb647
Robby <ta7382@gmail.com>
2019-08-17 17:55:00 +0800

    Fix url thumnailurl

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,8 @@
 const APIUrl = 'https://browndust-global-api.pmang.cloud/book/getAllCharacters';
 
-const thumbnailUrl = 'https://ic-common.pmang.cloud/static/bdt_book/thumbnail';
+// origin url, but cross-origin permission
+// const thumbnailUrl = 'https://ic-common.pmang.cloud/static/bdt_book/thumbnail';
+const thumbnailUrl = '/resource/thumbnail';
 
 export {
     APIUrl,
