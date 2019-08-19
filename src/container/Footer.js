a5ddd5f071d22d839d9709b60877b333ecbcb732
Robby <ta7382@gmail.com>
2019-08-19 20:08:01 +0800

    Update tips

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -8,14 +8,25 @@ import { FaGithub } from "react-icons/fa";
 import { GoIssueOpened } from "react-icons/go";
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.t = this.props.t;
+  }
+
   render() {
     return (
       <footer className='footer'>
+        <div className='footer-tip'>
+          <span>1. {this.t('tip1')}</span>
+          <span>2. {this.t('tip2')}</span>
+          <span>3. {this.t('tip3')}</span>
+          <span>4. {this.t('tip4')}</span>
+        </div>
         <div className='footer-items'>
           <a className='footer-link' alt='github' title='github' href='https://github.com/explooosion/browndust-share' target='_blank' rel="noopener noreferrer"><FaGithub size='2em' /></a>
           <a className='footer-link' alt='issue' title='issue' href='https://github.com/explooosion/browndust-share/issues' target='_blank' rel="noopener noreferrer"><GoIssueOpened size='2.3em' /></a>
         </div>
-        <span>This site is fan-made and not affiliated with NEOWIZ and GAMFS in any way.</span>
+        <span className='footer-copyright'>This site is fan-made and not affiliated with NEOWIZ and GAMFS in any way.</span>
       </footer>
     );
   }
