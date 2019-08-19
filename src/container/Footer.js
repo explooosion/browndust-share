9cece003a2118fd9cead599835b1d5f4675c5f76
Robby <ta7382@gmail.com>
2019-08-19 16:17:23 +0800

    Add copyright

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -11,8 +11,11 @@ class Footer extends Component {
   render() {
     return (
       <footer className='footer'>
-        <a className='footer-item' alt='github' title='github' href='https://github.com/explooosion/browndust-share' target='_blank' rel="noopener noreferrer"><FaGithub size='2em' /></a>
-        <a className='footer-item' alt='issue' title='issue' href='https://github.com/explooosion/browndust-share/issues' target='_blank' rel="noopener noreferrer"><GoIssueOpened size='2.3em' /></a>
+        <div className='footer-items'>
+          <a className='footer-link' alt='github' title='github' href='https://github.com/explooosion/browndust-share' target='_blank' rel="noopener noreferrer"><FaGithub size='2em' /></a>
+          <a className='footer-link' alt='issue' title='issue' href='https://github.com/explooosion/browndust-share/issues' target='_blank' rel="noopener noreferrer"><GoIssueOpened size='2.3em' /></a>
+        </div>
+        <span>This site is fan-made and not affiliated with NEOWIZ and GAMFS in any way.</span>
       </footer>
     );
   }
