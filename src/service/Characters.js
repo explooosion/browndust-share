a37321d63e6cef46ca517fff83104fed87feb5b3
Robby <ta7382@gmail.com>
2019-08-22 12:23:34 +0800

    Fix api 404

--- src/service/Characters.js
+++ src/service/Characters.js
@@ -1,3 +1,11 @@
 import { APIUrl } from '../config/api';
 
-export const getCharacters = async () => fetch(APIUrl).then(data => data.json() || [])
+export const getCharacters = async () => {
+    return fetch(APIUrl)
+        .then(data => data.json())
+        .catch(e => {
+            console.log('error', e);
+            console.log('change API url to dev');
+            return fetch(process.env.REACT_APP_API_URL_DEV).then(d => d.json())
+        })
+}
