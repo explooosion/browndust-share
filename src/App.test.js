65df26d6a62718f8243c1058986a8b5c2f1e894c
explooosion <ta7382@gmail.com>
2019-09-04 15:26:44 +0800

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from './App';
+import './index.scss';
+import './i18n';
+import reducers from './reducers';
+
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , div);
   ReactDOM.unmountComponentAtNode(div);
 });
