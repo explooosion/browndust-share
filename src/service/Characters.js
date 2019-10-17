a4468a584fd20513202b1427b4cc71c2baf2bc28
explooosion <ta7382@gmail.com>
2019-10-17 22:41:23 +0800

    Fix request failed for update

--- src/service/Characters.js
+++ src/service/Characters.js
@@ -1,21 +1,17 @@
 import { APIUrl } from '../config/api';
 
 export const getCharacters = async () => {
-    return fetch(APIUrl)
-        .then(data => data.json())
-        .catch(e => {
-            console.log('error', e);
-            console.log('change API url to dev');
-            return fetch(process.env.REACT_APP_API_URL_DEV).then(d => d.json())
-        })
+  return fetch(APIUrl)
+    .then(response => response.ok
+      ? response.json()
+      : console.warn('Change api url from github.') || fetch(process.env.REACT_APP_API_URL_DEV).then(d => d.json())
+    )
 }
 
 export const getCharactersGlobal = async () => {
-    return fetch(process.env.REACT_APP_API_URL_GLOBAL)
-        .then(data => data.json())
-        .catch(e => {
-            console.log('error', e);
-            console.log('change API url to dev');
-            return fetch(process.env.REACT_APP_API_URL_DEV).then(d => d.json())
-        })
+  return fetch(process.env.REACT_APP_API_URL_GLOBAL)
+    .then(response => response.ok
+      ? response.json()
+      : console.warn('Change api url from github.') || fetch(process.env.REACT_APP_API_URL_DEV).then(d => d.json())
+    )
 }
