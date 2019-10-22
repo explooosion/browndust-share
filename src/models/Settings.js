3fc5e31fbb5b6d91ad781576d07139c7009e7e6b
explooosion <ta7382@gmail.com>
2019-10-22 20:39:06 +0800

    Fix locale text, font family

--- src/models/Settings.js
+++ src/models/Settings.js
@@ -4,5 +4,5 @@ export default {
   locale: get('locale') === undefined ? 'US' : get('locale'),
   countries: ['US', 'TW', 'CN', 'KR', 'JP'],
   // language rule: https://github.com/ekwonye-richard/react-flags-select/blob/master/src/countries.js
-  customLabels: { 'US': 'English', 'TW': 'Traditional Chinese', 'CN': 'Simplified Chinese', 'KR': 'Korea', 'JP': 'Japan' },
+  customLabels: { 'US': 'English (en-US)', 'TW': '正體中文 (zh-TW)', 'CN': '简体中文 (zh-CN)', 'KR': '대한민국 (ko-KR)', 'JP': '日本 (ja-JP)' },
 };
