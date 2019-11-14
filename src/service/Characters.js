92388d165d2511b18777abcf9eea4d2204b130ee
robby <ta7382@gmail.com>
2019-11-14 13:49:17 +0800

    Add global json file for 404 use

--- src/service/Characters.js
+++ src/service/Characters.js
@@ -12,6 +12,6 @@ export const getCharactersGlobal = async () => {
   return fetch(process.env.REACT_APP_API_URL_GLOBAL)
     .then(response => response.ok
       ? response.json()
-      : console.warn('Change api url from github.') || fetch(process.env.REACT_APP_API_URL_DEV).then(d => d.json())
+      : console.warn('Change global api url from github.') || fetch(process.env.REACT_APP_API_URL_GLOBAL_DEV).then(d => d.json())
     )
 }
