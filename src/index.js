d5870dfe95b10e20e4a0a6518fca65247825b6c6
robby <ta7382@gmail.com>
2019-11-14 10:36:02 +0800

    Add stylelint, styled components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { ThemeProvider } from 'styled-components';
 import './index.scss';
 import './i18n';
 import reducers from './reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+const theme = require('sass-extract-loader?{"plugins":["sass-extract-js"]}!./scss/_index.scss');
 
 const store = createStore(
   reducers,
@@ -15,9 +19,11 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
