b7897cf796291956160a37cc9683b766a25ea660
robby <ta7382@gmail.com>
2020-01-03 13:05:44 +0800

    Remove serviceworker

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import './index.scss';
 import './i18n';
 import reducers from './reducers';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 const theme = require('sass-extract-loader?{"plugins":["sass-extract-js"]}!./scss/_index.scss');
@@ -30,8 +29,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
