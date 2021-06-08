766a8483fad86c95a7ca846e5e273417021f932f
Robby <ta7382@gmail.com>
2021-06-08 19:51:39 +0800

    Save getAll.json

--- pre-work/tool.js
+++ pre-work/tool.js
@@ -9,7 +9,7 @@ const URL = 'http://ic-common.pmang.cloud/static/bdt_book/thumbnail/';
 const API_CHARACTERS_KOREA = 'https://browndust-api.pmang.cloud/v1/book/character/getAll';
 // const API_CHARACTERS_KOREA = 'https://raw.githubusercontent.com/explooosion/browndust-share/6cf779be53f46117cb7aed659ea0d0f0c829751c/pre-work/getAll.json';
 
-const API_CHARACTERS_FILE_NAME = `getAll_${moment().format('YYYYMMDDTHHmmSSS')}.json`;
+const API_CHARACTERS_FILE_NAME = `getAll.json`;
 
 https.request(API_CHARACTERS_KOREA, response => {
   const data = new Stream();
@@ -27,26 +27,26 @@ function downloadThumbnails(error) {
   const thumbnails = require(`./${API_CHARACTERS_FILE_NAME}`);
   Promise.all(
     thumbnails
-      .map(d => {
-        return {
-          url: `${URL}${d._uiIconImageName.split('*')[1]}.png`,
-          name: `${d._uiIconImageName.split('*')[1]}.png`,
-        }
-      })
-      .forEach(({ url, name }, index, arr) => {
+      .map(d => ({
+        url: `${URL}${d._uiIconImageName.split('*')[1]}.png`,
+        name: `${d._uiIconImageName.split('*')[1]}.png`,
+      }))
+      .map(({ url, name }, index, arr) => {
         try {
           http.request(url, response => {
             const data = new Stream();
             console.log(`parse:${url}`);
             response.on('data', chunk => data.push(chunk));
-            response.on('end', () => fs.writeFileSync(`./public/resource/thumbnail/${name}`, data.read()));
+            response.on('end', () => {
+              fs.writeFileSync(`./public/resource/thumbnail/${name}`, data.read());
+            });
           }).end();
         } catch (error) {
           console.log('thumbnails', error);
         }
       })
   ).then(() => {
-    fs.unlinkSync(`${__dirname}/${API_CHARACTERS_FILE_NAME}`);
+    // fs.unlinkSync(`${__dirname}/${API_CHARACTERS_FILE_NAME}`);
   });
 }
 
