30f2c67407386f2a24ef678ee607cfff636a6fa7
robby <ta7382@gmail.com>
2023-05-05 20:16:25 +0800

    Add 6 star filter for mythic, legend

--- src/container/List.js
+++ src/container/List.js
@@ -8,7 +8,7 @@ import './List.scss';
 import { withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { Checkbox } from 'pretty-checkbox-react';
-import { FaStar } from "react-icons/fa";
+import { FaStar } from 'react-icons/fa';
 import { css } from '@emotion/react';
 import HashLoader from 'react-spinners/HashLoader';
 import { sify } from 'chinese-conv';
@@ -18,7 +18,6 @@ import { getIconUrlByTypeId } from '../utils';
 import { updateDataset } from '../actions';
 
 const override = css`
-
   display: block;
   margin: 20vh auto 0;
 `;
@@ -33,8 +32,9 @@ class List extends Component {
       search: '',
       type: 1,
       stars: [
+        { label: '6', checked: true, star: 6 },
         { label: '5', checked: true, star: 5 },
-        { label: '4', checked: true, star: 4 },
+        { label: '4', checked: false, star: 4 },
         { label: '3', checked: false, star: 3 },
         { label: this.t('normal'), checked: false, star: 2 },
       ],
@@ -66,37 +66,45 @@ class List extends Component {
       ],
       stars,
       nameOptions,
-    }
+    };
   }
 
-  onStarChange = (_label) => {
+  onStarChange = _label => {
     const stars = this.state.stars;
     const star = stars.find(({ label }) => label === _label);
     star.checked = !star.checked;
     this.setState({ stars });
-  }
+  };
 
-  onNameOptionsChange = (_label) => {
+  onNameOptionsChange = _label => {
     const nameOptions = this.state.nameOptions.map(n =>
-      (n.label === _label) ? { ...n, checked: !n.checked } : n
+      n.label === _label ? { ...n, checked: !n.checked } : n
     );
     this.setState({ nameOptions });
-  }
+  };
 
-  onRemoveByDrop = (ev) => {
+  onRemoveByDrop = ev => {
     const sid = ev.dataTransfer.getData('sid');
     if (sid === '0') return;
-    const formation = this.props.dataset.formation.map((f) => {
+    const formation = this.props.dataset.formation.map(f => {
       return sid !== f.id
         ? f
-        : { ...f, type: 0, backgroundImage: null, uniqueCode: 0, dragOver: false, queue: 0, level: 0 };
+        : {
+            ...f,
+            type: 0,
+            backgroundImage: null,
+            uniqueCode: 0,
+            dragOver: false,
+            queue: 0,
+            level: 0,
+          };
     });
-    this.dispatch(updateDataset({ formation }))
-  }
+    this.dispatch(updateDataset({ formation }));
+  };
 
-  onDragOver = (ev) => {
+  onDragOver = ev => {
     ev.preventDefault();
-  }
+  };
 
   getListBySearch = (search = '', characters) => {
     if (search === '') return characters;
@@ -112,31 +120,47 @@ class List extends Component {
         _charName_TH,
       } = c;
       let match = false;
-      console.log(search, _charName_TW, _charName_TW.indexOf(search))
+      console.log(search, _charName_TW, _charName_TW.indexOf(search));
       switch (this.props.settings.locale) {
-        case 'US': match = _charName_ENG.indexOf(search) > -1; break;
-        case 'ES': match = _charName_SPA.indexOf(search) > -1; break;
-        case 'DE': match = _charName_GER.indexOf(search) > -1; break;
-        case 'TW': match = _charName_TW.indexOf(search) > -1 || sify(_charName_TW).indexOf(search) > -1; break;
-        case 'CN': match = _charName_TW.indexOf(search) > -1 || sify(_charName_TW).indexOf(search) > -1; break;
-        case 'JP': match = _charName_JAP.indexOf(search) > -1; break;
-        case 'KR': match = _charName.indexOf(search) > -1; break;
-        case 'TH': match = _charName_TH.indexOf(search) > -1; break;
-        default: match = true;
+        case 'US':
+          match = _charName_ENG.indexOf(search) > -1;
+          break;
+        case 'ES':
+          match = _charName_SPA.indexOf(search) > -1;
+          break;
+        case 'DE':
+          match = _charName_GER.indexOf(search) > -1;
+          break;
+        case 'TW':
+          match = _charName_TW.indexOf(search) > -1 || sify(_charName_TW).indexOf(search) > -1;
+          break;
+        case 'CN':
+          match = _charName_TW.indexOf(search) > -1 || sify(_charName_TW).indexOf(search) > -1;
+          break;
+        case 'JP':
+          match = _charName_JAP.indexOf(search) > -1;
+          break;
+        case 'KR':
+          match = _charName.indexOf(search) > -1;
+          break;
+        case 'TH':
+          match = _charName_TH.indexOf(search) > -1;
+          break;
+        default:
+          match = true;
       }
       return match;
     });
-  }
+  };
 
   /**
    * Render charactor types
    */
   renderTypes() {
-    return this.state.types.map(({ type, label }) =>
-    (
+    return this.state.types.map(({ type, label }) => (
       <button
         key={`type-${label}`}
-        type='button'
+        type="button"
         className={`type  ${type === this.state.type ? 'active' : ''}`}
         onClick={() => this.setState({ type })}
       >
@@ -150,13 +174,12 @@ class List extends Component {
    * Render stars filter
    */
   renderFilterStars() {
-    return this.state.stars.map(({ label, checked }, index) =>
-    (
+    return this.state.stars.map(({ label, checked }, index) => (
       <Checkbox
         key={`start-${index}`}
-        shape='round'
-        color='info'
-        animation='jelly'
+        shape="round"
+        color="info"
+        animation="jelly"
         icon={<i className="mdi mdi-check" />}
         checked={checked}
         onChange={() => this.onStarChange(label)}
@@ -173,9 +196,9 @@ class List extends Component {
     return this.state.nameOptions.map(({ label, checked }, index) => (
       <Checkbox
         key={`name-${index}`}
-        shape='round'
-        color='info'
-        animation='jelly'
+        shape="round"
+        color="info"
+        animation="jelly"
         icon={<i className="mdi mdi-check" />}
         checked={checked}
         onChange={() => this.onNameOptionsChange(label)}
@@ -190,26 +213,25 @@ class List extends Component {
    */
   renderCharacters(characters) {
     return this.getListBySearch(this.state.search, characters)
-      .filter(({ _type, _growType, _star }) => {
+      .filter(character => {
+        const { _type, _growType, _star } = character;
         return (
           Number(_type) === this.state.type &&
           Number(_growType) >= 0 &&
-          (
-            this.state.stars[0].checked && Number(_star) === 5 ||
-            this.state.stars[1].checked && Number(_star) === 4 ||
-            this.state.stars[2].checked && Number(_star) === 3 ||
-            this.state.stars[3].checked && Number(_star) === 2 ||
-            this.state.stars[3].checked && Number(_star) === 1
-          )
-        )
+          ((this.state.stars[0].checked && Number(_star) === 6) ||
+            (this.state.stars[1].checked && Number(_star) === 5) ||
+            (this.state.stars[2].checked && Number(_star) === 4) ||
+            (this.state.stars[3].checked && Number(_star) === 3) ||
+            (this.state.stars[4].checked && Number(_star) === 2) ||
+            (this.state.stars[4].checked && Number(_star) === 1))
+        );
       })
-      .map((c, index) => (
-        <Mercenary
-          key={`mercenary-${index}`}
-          params={c}
-          nameOptions={this.state.nameOptions}
-        />
-      ))
+      .map((c, index) => {
+        console.log(c);
+        return (
+          <Mercenary key={`mercenary-${index}`} params={c} nameOptions={this.state.nameOptions} />
+        );
+      });
   }
 
   render() {
@@ -217,43 +239,39 @@ class List extends Component {
     const characters = this.props.characters;
     return (
       <div
-        className='list'
-        onDragOver={(e) => this.onDragOver(e)}
-        onDrop={(e) => this.onRemoveByDrop(e)}
+        className="list"
+        onDragOver={e => this.onDragOver(e)}
+        onDrop={e => this.onRemoveByDrop(e)}
       >
-        <div className='types'>
-          {this.renderTypes()}
-        </div>
-        <div className='filter'>
-          <div className='star'>
-            <span className='filter-title'><FaStar /> {this.t('star')}：</span>
+        <div className="types">{this.renderTypes()}</div>
+        <div className="filter">
+          <div className="star">
+            <span className="filter-title">
+              <FaStar /> {this.t('star')}：
+            </span>
             {this.renderFilterStars()}
           </div>
-          <div className='name'>
-            <span className='filter-title'>{this.t('name')}：</span>
+          <div className="name">
+            <span className="filter-title">{this.t('name')}：</span>
             {this.renderFilterNameOptions()}
           </div>
         </div>
-        <div className='search'>
+        <div className="search">
           <input
-            className='search-text'
-            type='text'
+            className="search-text"
+            type="text"
             placeholder={this.t('search')}
             value={this.state.search}
-            onChange={(e) => this.setState({ search: e.target.value })}
+            onChange={e => this.setState({ search: e.target.value })}
           />
         </div>
 
-        <div className='content'>
-          {
-            characters.length === 0 ? (
-              <HashLoader
-                css={override}
-                color={'#5ac0de'}
-                size={100}
-              />
-            ) : this.renderCharacters(characters)
-          }
+        <div className="content">
+          {characters.length === 0 ? (
+            <HashLoader css={override} color={'#5ac0de'} size={100} />
+          ) : (
+            this.renderCharacters(characters)
+          )}
         </div>
       </div>
     );
@@ -266,7 +284,7 @@ const mapStateToProps = state => {
     charactersGlobal: state.charactersGlobal,
     dataset: state.dataset,
     settings: state.settings,
-  }
-}
+  };
+};
 
 export default withTranslation()(connect(mapStateToProps)(List));
